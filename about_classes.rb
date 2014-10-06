4551e4df26d592e88e4fb09325e065b77b36a77a
Sherry Seibel <sherry.seibel@gmail.com>
2014-10-06 18:50:34 -0400

    Fix typo in about_classes

--- about_classes.rb
+++ about_classes.rb
@@ -184,6 +184,6 @@ class AboutClasses < Neo::Koan
     assert_equal "[1, 2, 3]", array.inspect
 
     assert_equal "STRING", "STRING".to_s
-    assert_equal "\STRING\"", "STRING".inspect
+    assert_equal "\"STRING\"", "STRING".inspect
   end
 end
